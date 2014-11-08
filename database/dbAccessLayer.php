8419f7ab6a685bb9bee09f825f77204b9b6d8536
avinav <sharan.avinav@gmail.com>
2014-11-08 10:35:43 -0500

    dbAccessLayer: addorganization count increase user

--- database/dbAccessLayer.php
+++ database/dbAccessLayer.php
@@ -23,19 +23,27 @@ function insert_user($username, $pass, $first, $last, $org, $type)
 	insert_doc($user, $doc);	
 }
 
-function insertOrganization($id,$org_name, $org_type, $user_count) {
+function insertOrganization($org_name, $org_type, $user_count) {
 	global $org;
+	$res = $org->find(array(),array("_id"=>1))->sort(array("_id" => -1))->limit(1);
+	foreach($res as $r) {
+		$maxid = $r["_id"];
+	}
+	$id = $maxid + 1;
 	$doc = array(
-			"_id" => $id, "org_name" => $org_name, "org_type" => $org_type,
+		"_id" => $id, "org_name" => $org_name, "org_type" => $org_type,
 			"user_count" => $user_count
 	);
 	insert_doc($org,$doc);
 }
 
 function addUserOrganization($user_id,$org_id, $user_type) {
-	global $user;
+	global $user,$org;
 	$doc = array("user_association" => array(array("org_id" => $org_id, "user_type" => $user_type)));
 	$user->update(array("_id" => $user_id),array('$push' => $doc));
+	$res = $org->findOne(array("_id" => $org_id),array());
+	$count = $res["user_count"];
+	$org->update(array("_id" => $org_id),array('$set' => array("user_count" => $count + 1)));
 }
 insert_user("admn3", "admn3", "fce", "bok3", "mak3", "student6");
 ?>
