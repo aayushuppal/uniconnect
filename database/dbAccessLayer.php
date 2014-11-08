af3aa2cdbe396f1a4de08c0ee0c056278ecfee78
zstring <jai.himanshu001@gmail.com>
2014-11-08 09:18:27 -0500

    Fixed bugs in inserting data into user collection

--- database/dbAccessLayer.php
+++ database/dbAccessLayer.php
@@ -1,14 +1,11 @@
 <?php
-function dbConn() 
-{
+
 	$conn = new MongoClient ( "ec2-54-68-28-28.us-west-2.compute.amazonaws.com:12345" );
 	$db = $conn->kuchbhi;
-	global $user,$org,$ques,$ans;
 	$user = $db->user;
 	$org = $db->organization;
 	$ques = $db->question;
 	$ans = $db->answer;	
-}
 
 
 function insert_doc($col, $doc) 
@@ -21,8 +18,9 @@ function insert_user($username, $pass, $first, $last, $org, $type)
 	$doc = array("username" => $username, "password" => $pass,
 					"first_name" => $first, "last_name" => $last,
 					"user_association" => array("org_id" => $org, 
-											"user_type" => $type));
+												"user_type" => $type));
+	global $user;
 	insert_doc($user, $doc);	
 }
-
+insert_user("admin1", "admin1", "face", "book", "mark", "student");
 ?>
